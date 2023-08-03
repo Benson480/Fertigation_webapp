7c786b5d734165e17958cb085512317fed70e7ec
Benson Mwangi <bensonmwangi101@gmail.com>
2023-08-03 10:12:08 +0300

    c1

--- myproject/settings.py
+++ myproject/settings.py
@@ -193,19 +193,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/mystaticfiles/'
-# STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'productionfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-# STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
-
-STATICFILES_DIRS = [
-    BASE_DIR / 'mystaticfiles',
-]
 
 # STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "mystaticfiles"),
+#     BASE_DIR / 'mystaticfiles',
 # ]
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "mystaticfiles"),
+]
 
 WHITENOISE_MANIFEST_STRICT = False
 
