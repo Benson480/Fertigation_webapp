9c05bf067e0b6d533ecf2f9b2dabbc5fa2907bd3
Benson Mwangi <bmwangi@SBA.LOCAL>
2023-09-11 20:44:02 +0300

    Update

---
+++ dependency.bat
@@ -0,0 +1,10 @@
+pip install django-admin interface
+pip install PyYAML
+pip install django-crispy-forms
+pip install django-flat-responsive
+pip install django-flat-theme
+pip install django-admin-menu
+pip install django-axes
+pip install django-crispy_bootstrap
+pip install crispy_bootstrap4
+pip install django-widget-tweaks
