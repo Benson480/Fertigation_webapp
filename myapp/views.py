4eec0c7aff5cdf5e96936875c80514aedc2604fb
Benson Mwangi <bmwangi@SBA.LOCAL>
2023-09-17 21:35:07 +0300

    Coding both systems

--- myapp/views.py
+++ myapp/views.py
@@ -36,29 +36,30 @@ def regenerate_csrf_token(request):
         csrf_token = get_token(request)
         return HttpResponse(csrf_token)
 
+
 def register_view(request):
-   pass
-    # if request.method == 'POST':
-    #     signup_form = SignupForm(request.POST)
+    if request.method == 'POST':
+        signup_form = SignupForm(request.POST)
 
-    #     if signup_form.is_valid():
-    #         username = signup_form.cleaned_data['username']
-    #         email = signup_form.cleaned_data['email']
-    #         password = signup_form.cleaned_data['password']
+        if signup_form.is_valid():
+            username = signup_form.cleaned_data['username']
+            email = signup_form.cleaned_data['email']
+            password = signup_form.cleaned_data['password']
             
-    #         # Check if the username is unique
-    #         if User.objects.filter(username=username).exists():
-    #             signup_form.add_error('username', 'This username is already taken.')
-    #         else:
-    #             user = User.objects.create_user(username=username, email=email, password=password)
-    #             user = authenticate(request, username=username, password=password)
-    #             login(request, user)
-    #             return redirect('login')  # Redirect to the desired URL after successful signup
-    # else:
-    #     signup_form = SignupForm()
+            # Check if the username is unique
+            if User.objects.filter(username=username).exists():
+                signup_form.add_error('username', 'This username is already taken.')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user = authenticate(request, username=username, password=password)
+                login(request, user)
+                return redirect('login')  # Redirect to the desired URL after successful signup
+    else:
+        signup_form = SignupForm()
 
-    # return render(request, 'accounts/register.html', {'signup_form': signup_form})
+    return render(request, 'accounts/register.html', {'signup_form': signup_form})
     
+
 
 logger = logging.getLogger(__name__)
 
@@ -93,8 +94,6 @@ def login_view(request):
 
         if User.objects.filter(username=username).exists():
             messages.error(request, f"This Username {request.POST.get('username')} is already in taken!")
-        elif User.objects.filter(email=email).exists():
-              messages.error(request, f"This Email '{email}' is already associated with an account!")
         else:
             user = User.objects.create_user(username=username, email=email, password=password)
             user = authenticate(request, username=username, password=password)
@@ -123,31 +122,6 @@ def login_view(request):
 
 
 
-
-
-"""Currently this view is not in use, Reason it is hard to implement and has blocked
-  Me from the system for several times
-"""
-# def login_view(request):
-#     error_message = None
-    
-#     if request.method == 'POST':
-#         login_form = LoginForm(request.POST)
-#         if login_form.is_valid():
-#             email = login_form.cleaned_data.get('email')  # Use lowercase field name
-#             password = login_form.cleaned_data.get('password')  # Use lowercase field name
-#             user = authenticate(username=email, password=password)
-#             if user is not None:
-#                 login(request, user)
-#                 return redirect('/dashboard/')
-#             else:
-#                 error_message = "Invalid login credentials"
-#         else:
-#             error_message = "Form validation failed"
-#     else:
-#         login_form = LoginForm()
-
-#     return render(request, 'accounts/login.html', {'form': login_form, 'error_message': error_message})
 
 def logout_view(request):
     if request.method == "POST":
